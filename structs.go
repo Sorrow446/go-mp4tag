d93f2850abbd59396f6affb589539b7d6ca54bb8
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2022-06-12 08:19:17 +0100

    Add files via upload

---
+++ structs.go
@@ -0,0 +1,20 @@
+package mp4tag
+
+type Tags struct {
+	Album       string
+	AlbumArtist string
+	Artist      string
+	Comment     string
+	Composer    string
+	Cover       []byte
+	Custom      map[string]string
+	Delete      []string
+	DiskNumber  int
+	DiskTotal   int
+	Genre       string
+	Label       string
+	Title       string
+	TrackNumber int
+	TrackTotal  int
+	Year        string
+}
